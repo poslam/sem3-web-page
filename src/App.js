95b00d91ff4ed0caaa32ecf255b49e19a35ee691
deniskirienko <kirden04@gmail.com>
2023-10-18 12:56:13 +1000

    upd2: gif and restyling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import React, { Component } from 'react';
-import back_img from './static/background.jpeg'
+import pop_cat from './media/pop-cat.gif'
 
-class App extends Component {
-render() {
-	const myStyle={
-		backgroundImage: `url(${back_img})`,
-		height:'100vh',
-		marginTop:'-70px',
-		fontSize:'50px',
-		backgroundSize: 'cover',
-		backgroundRepeat: 'no-repeat',
-    
-	};
-	return (
-	<div style={myStyle}>
-		<h4> Кириенко Денис </h4>
-	</div>
-	);
-}
+function App() {
+  return (
+    <>
+      <div>
+        <>
+        <h1 style={{display: 'flex', justifyContent: 'center', 
+              position: 'relative', left: 100}}>Кириенко Денис</h1>
+          <p style={{display: 'flex', justifyContent: 'center',
+              position: 'relative', left: 100}}>Backend-разработчик</p>
+          <p style={{display: 'flex', justifyContent: 'center',
+              position: 'relative', left: 100}}>+7 924 522 71 72&nbsp; &nbsp; &nbsp;<a href="mailto:kirienko.do@dvfu.ru">kirienko.do@dvfu.ru</a>&nbsp; &nbsp; &nbsp; @poslam</p>
+          <p style={{display: 'flex', justifyContent: 'center',
+              position: 'relative', left: 100}}>&nbsp;</p>
+          <p style={{display: 'flex', justifyContent: 'center',
+              position: 'relative', left: 100}}>&nbsp;</p>
+        </>
+      </div>
+      <div>
+        <><img src={pop_cat} alt="loading..."/></>
+      </div>
+    </>
+  );
 }
 
 export default App;
